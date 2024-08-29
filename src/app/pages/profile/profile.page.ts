c448f034cb5e1e741fe74cf45b5cd6747a3992a9
Martin Arroyo <ma.arroyoq@duocuc.cl>
2024-08-29 15:55:24 -0400

    Commit Perfil

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
 })
-export class ProfilePage implements OnInit {
+export class ProfilePage {
 
-  constructor() { }
+  username: string = 'Invitado';
+  name!: string;
+  lastname!: string;
+  edLevel!: string;
+  birthday!: string;
 
-  ngOnInit() {
+  constructor(
+    private router: Router,
+    private alertController: AlertController
+  ) {
+    let state = this.router.getCurrentNavigation()?.extras?.state;
+    if (state) {
+      this.username = state['username'];
+    }
+   }
+
+   async showInfo() {
+    const alert = await this.alertController.create({
+      header: 'Alerta!',
+      message: 'Hola ' + this.name + ' '+ this.lastname + '!',
+    });
+    alert.present();
   }
+
+  clear(){
+    this.birthday = '';
+    this.name = '';
+    this.lastname = '';
+    this.edLevel = '';
+  }
+  
 
 }
